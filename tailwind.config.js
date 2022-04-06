4f477887e283c13acdd00bb089144495eaafa7f2
jaygarshong <jason.garshong@mybigpay.net>
2022-04-06 10:44:55 +0000

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    fontFamily: {
+      sans: ['Jura', 'sans-serif']
+    },
+    extend: {
+      colors:{
+        'primary': "#1f438d",
+        'secondary': "#79c143",
+        'textcolor': "#6d6e71",
+      },
+      backgroundImage: {
+        'main-bg': "url('/src/assets/bg-colour.png')",
+      }
+    },
+  },
+  plugins: [],
+}
