704928a090b510322048509891c787b9604e4c98
jaygarshong <jason.garshong@mybigpay.net>
2022-04-14 10:11:08 +0000

    layout complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    /* screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    }, */
     fontFamily: {
       sans: ['Jura', 'sans-serif']
     },
     extend: {
-      colors:{
+      colors: {
         'primary': "#1f438d",
         'secondary': "#79c143",
         'textcolor': "#6d6e71",
