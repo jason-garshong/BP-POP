9f79c89817eb41430c996907f5de9c98c172fa36
jaygarshong <jason.garshong@mybigpay.net>
2022-04-14 09:12:32 +0000

    styles cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
+    /* screens: {
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
-    },
+    }, */
     fontFamily: {
       sans: ['Jura', 'sans-serif']
     },
@@ -19,7 +19,7 @@ module.exports = {
       },
       backgroundImage: {
         'main-bg': "url('/src/assets/bg_colour.png')",
-      }
+      },
     },
   },
   plugins: [],
